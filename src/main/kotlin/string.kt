70975c232021de4ddc45ab7639409266ed215ae6
SarikaKhoje <sarikakhoje894@gmail.com>
2025-07-26 12:16:25 +0530

    Commit

---
+++ src/main/kotlin/string.kt
@@ -0,0 +1,17 @@
+fun printStrLength(str:String)
+{
+    if(str!=null)
+    {
+      println(str.length)
+    }
+    else
+    {
+        println("string is null")
+    }
+
+
+}
+fun main()
+{
+    printStrLength(str="sarika")
+}
