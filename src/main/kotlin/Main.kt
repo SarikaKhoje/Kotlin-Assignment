70dae317e7997eb3a21de3f981208872cd2309ba
SarikaKhoje <sarikakhoje894@gmail.com>
2025-07-26 12:01:31 +0530

    Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 fun main()
 {
     println("sanjivani college of enginnering")
+    println("sanjivani college of enginnering")
+    println("sanjivani college of enginnering")
+    println("sanjivani college of enginnering")
+    println("sanjivani college of enginnering")
 }
