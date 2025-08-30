4ac0503023395e747d9b603b9b2cd4d12e058834
SarikaKhoje <sarikakhoje894@gmail.com>
2025-08-30 08:57:47 +0530

    next ass

---
+++ src/main/kotlin/ass-2.kt
@@ -0,0 +1,16 @@
+fun printStrLength(str:String)
+{
+    if(str!=null)
+    {
+      println(str.length)
+    }
+    else
+    {
+        println("string is null")
+    }
+}
+fun main()
+{
+
+    printStrLength(str="sarika")
+}
